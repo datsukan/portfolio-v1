856447aca867c62ad8bbc2be7025deb1596db92c
datsukan <blue.earth.3543@gmail.com>
2020-05-25 20:34:07 +0900

    [修正] ポートフォリオの画像差し替え

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,7 +5,7 @@
         <h3 class="display-2 font-weight-bold orange--text mb-10">Portfolio</h3>
         <v-row align="center" justify="center">
           <v-col cols="12" md="6" lg="4">
-            <v-dialog v-model="dialog1" width="1000">
+            <v-dialog v-model="dialog1" width="1300">
               <template v-slot:activator="{ on }">
                 <v-card width="400" height="250" class="mx-auto" v-on="on">
                   <v-img src="../assets/contents/portfolio1.png" width="400" height="250"></v-img>
@@ -22,7 +22,12 @@
                 <v-card-text>
                   <v-row dense>
                     <v-col cols="12" md="6">
-                      <v-carousel cycle interval="5000" height="400" hide-delimiter-background show-arrows-on-hover>
+                      <v-carousel
+                        cycle
+                        interval="5000"
+                        hide-delimiter-background
+                        show-arrows-on-hover
+                      >
                         <v-carousel-item :key="1">
                           <v-sheet height="100%">
                             <v-row class="fill-height" align="center" justify="center">
@@ -49,11 +54,11 @@
                     <v-col cols="12" md="6" class="px-4">
                       <p class="title font-weight-bold">概要</p>
                       <p>
-                        私のポートフォリオサイトです。<br />
-                        Vue.js + Vuetifyをメインに使用したSPA（シングルページアプリケーション）になっています。<br />
-                        モダンJavaScriptを使用してフロントリッチな実装にすることで、UI/UXを高めています。<br />
-                        また、AWS S3を用いて簡易的にサーバーレスを実現しました。<br />
-                        UIコンポーネントを上手く活用してできるかぎりオシャレで見やすいサイトを目指しました。
+                        私のポートフォリオサイトです。
+                        <br />Vue.js + Vuetifyをメインに使用したSPA（シングルページアプリケーション）になっています。
+                        <br />モダンJavaScriptを使用してフロントリッチな実装にすることで、UI/UXを高めています。
+                        <br />また、AWS S3を用いて簡易的にサーバーレスを実現しました。
+                        <br />UIコンポーネントを上手く活用してできるかぎりオシャレで見やすいサイトを目指しました。
                       </p>
                       <v-btn color="primary" href="/" target="_blank" outlined small>
                         ポートフォリオサイトへ
@@ -62,9 +67,7 @@
                       <v-divider class="my-2"></v-divider>
                       <p class="title font-weight-bold mb-0">使用技術</p>
                       <v-chip-group column>
-                        <v-chip v-for="tag in tags1" :key="tag" small>
-                          {{ tag }}
-                        </v-chip>
+                        <v-chip v-for="tag in tags1" :key="tag" small>{{ tag }}</v-chip>
                       </v-chip-group>
                     </v-col>
                   </v-row>
@@ -73,7 +76,7 @@
             </v-dialog>
           </v-col>
           <v-col cols="12" md="6" lg="4">
-            <v-dialog v-model="dialog2" width="1000">
+            <v-dialog v-model="dialog2" width="1300">
               <template v-slot:activator="{ on }">
                 <v-card width="400" height="250" class="mx-auto" v-on="on">
                   <v-img src="../assets/contents/portfolio2.png" width="400" height="250"></v-img>
@@ -81,7 +84,7 @@
               </template>
               <v-card>
                 <v-card-title>
-                  <span class="font-weight-bold info--text">Chatwork用 Chrome拡張機能</span>
+                  <span class="font-weight-bold info--text">Chrome拡張機能</span>
                   <v-spacer></v-spacer>
                   <v-btn color="info" text @click="dialog2 = false" icon>
                     <v-icon large>mdi-close-circle</v-icon>
@@ -90,7 +93,12 @@
                 <v-card-text>
                   <v-row dense>
                     <v-col cols="12" md="6">
-                      <v-carousel cycle interval="5000" height="400" hide-delimiter-background show-arrows-on-hover>
+                      <v-carousel
+                        cycle
+                        interval="5000"
+                        hide-delimiter-background
+                        show-arrows-on-hover
+                      >
                         <v-carousel-item :key="1">
                           <v-sheet height="100%">
                             <v-row class="fill-height" align="center" justify="center">
@@ -110,11 +118,11 @@
                     <v-col cols="12" md="6" class="px-4">
                       <p class="title font-weight-bold">概要</p>
                       <p>
-                        Chatworkに機能追加するChrome拡張機能です。<br />
-                        ReactのSPA（シングルページアプリケーション）で開発されているChatworkのUIに介入して、
-                        出退勤のメッセージをワンクリックで送信できるボタンを追加します。<br />
-                        社内の運用で実際に用いています。<br />
-                        Googleが提供している各種Chrome APIとJavaScript（jQuery）を用いて実装しました。
+                        Chatworkに機能追加するChrome拡張機能です。
+                        <br />ReactのSPA（シングルページアプリケーション）で開発されているChatworkのUIに介入して、
+                        出退勤のメッセージをワンクリックで送信できるボタンを追加します。
+                        <br />社内の運用で実際に用いています。
+                        <br />Googleが提供している各種Chrome APIとJavaScript（jQuery）を用いて実装しました。
                       </p>
                       <v-btn
                         color="primary"
@@ -129,9 +137,7 @@
                       <v-divider class="my-2"></v-divider>
                       <p class="title font-weight-bold mb-0">使用技術</p>
                       <v-chip-group column>
-                        <v-chip v-for="tag in tags2" :key="tag" small>
-                          {{ tag }}
-                        </v-chip>
+                        <v-chip v-for="tag in tags2" :key="tag" small>{{ tag }}</v-chip>
                       </v-chip-group>
                     </v-col>
                   </v-row>
@@ -140,7 +146,7 @@
             </v-dialog>
           </v-col>
           <v-col cols="12" md="6" lg="4">
-            <v-dialog v-model="dialog3" width="1000">
+            <v-dialog v-model="dialog3" width="1300">
               <template v-slot:activator="{ on }">
                 <v-card width="400" height="250" class="mx-auto" v-on="on">
                   <v-img src="../assets/contents/portfolio3.png" width="400" height="250"></v-img>
@@ -148,7 +154,7 @@
               </template>
               <v-card>
                 <v-card-title>
-                  <span class="font-weight-bold info--text">スキル情報共有アプリケーション</span>
+                  <span class="font-weight-bold info--text">スキル情報共有アプリ</span>
                   <v-spacer></v-spacer>
                   <v-btn color="info" text @click="dialog3 = false" icon>
                     <v-icon large>mdi-close-circle</v-icon>
@@ -157,7 +163,12 @@
                 <v-card-text>
                   <v-row dense>
                     <v-col cols="12" md="6">
-                      <v-carousel cycle interval="5000" height="400" hide-delimiter-background show-arrows-on-hover>
+                      <v-carousel
+                        cycle
+                        interval="5000"
+                        hide-delimiter-background
+                        show-arrows-on-hover
+                      >
                         <v-carousel-item :key="1">
                           <v-sheet height="100%">
                             <v-row class="fill-height" align="center" justify="center">
@@ -205,20 +216,18 @@
                     <v-col cols="12" md="6" class="px-4">
                       <p class="title font-weight-bold">概要</p>
                       <p>
-                        エンジニアやIT職種の人材のスキル情報を共有するためのWebアプリケーションです。<br />
-                        Laravel + Vue.js +
-                        Vuetifyをメインに使用したSPA（シングルページアプリケーション）になっています。<br />
-                        認証・ユーザー管理といった標準的な機能に加え、
-                        詳細かつ閲覧しやすい形式でスキル情報を登録・更新・参照可能になっています。<br />
-                        APIによる非同期通信を用いて全文検索や一括更新を快適な速度で処理できるパフォーマンスに調整しています。<br />
-                        現在も継続してバージョンアップを行っています。
+                        エンジニアやIT職種の人材のスキル情報を共有するためのWebアプリケーションです。
+                        <br />Laravel + Vue.js +
+                        Vuetifyをメインに使用したSPA（シングルページアプリケーション）になっています。
+                        <br />認証・ユーザー管理といった標準的な機能に加え、
+                        詳細かつ閲覧しやすい形式でスキル情報を登録・更新・参照可能になっています。
+                        <br />APIによる非同期通信を用いて全文検索や一括更新を快適な速度で処理できるパフォーマンスに調整しています。
+                        <br />現在も継続してバージョンアップを行っています。
                       </p>
                       <v-divider class="my-2"></v-divider>
                       <p class="title font-weight-bold mb-0">使用技術</p>
                       <v-chip-group column>
-                        <v-chip v-for="tag in tags3" :key="tag" small>
-                          {{ tag }}
-                        </v-chip>
+                        <v-chip v-for="tag in tags3" :key="tag" small>{{ tag }}</v-chip>
                       </v-chip-group>
                     </v-col>
                   </v-row>
